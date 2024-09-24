8700473183f03caa66055787bd90ef19b89b95e8
Dave Slack <davidkslack@yahoo.co.uk>
2024-09-24 09:33:07 +0100

    603: Changed the content.js to use snake case

--- data/content.js
+++ data/content.js
@@ -22,8 +22,8 @@ export async function getContent(url = '/') {
             'title': "Page not found",
             'heading': "Page not found",
             'body': "<p>Page not found.</p>",
-            'meta-title' : '404 | Page not found',
-            'meta-description' : 'Missing page',
+            'meta_title' : '404 | Page not found',
+            'meta_description' : 'Missing page',
             'error' : 404,
         }
     }
@@ -36,12 +36,12 @@ export async function getContent(url = '/') {
             'title': jsonResponse.title[0].value,
             'heading': jsonResponse.field_subtitle[0].value,
             'body': jsonResponse.body[0].processed,
-            'meta-title' : jsonResponse.metatag[0].attributes.content,
-            'meta-description' : jsonResponse.metatag[1].attributes.content,
+            'meta_title' : jsonResponse.metatag[0].attributes.content,
+            'meta_description' : jsonResponse.metatag[1].attributes.content,
             'created' : jsonResponse.created[0].value,
-            'background-colour': jsonResponse.field_background_colour[0].color,
-            'font-colour': jsonResponse.field_font_colour[0].color,
-            'main-image': jsonResponse.field_main_image[0],
+            'background_colour': jsonResponse.field_background_colour[0].color,
+            'font_colour': jsonResponse.field_font_colour[0].color,
+            'main_image': jsonResponse.field_main_image[0],
             'error' : null,
         }
     }catch(e){
@@ -49,8 +49,8 @@ export async function getContent(url = '/') {
             'title': "API issue",
             'heading': "The API is gone away, we're trying to find it",
             'body': "<p>We've lost our API and are looking for it, please try refreshing the page and if that doesn't work there is a larger issue, and we're looking into it</p>",
-            'meta-title' : '404 | API issue',
-            'meta-description' : 'Missing API',
+            'meta_title' : '404 | API issue',
+            'meta_description' : 'Missing API',
             'error' : 404,
         }
     }
