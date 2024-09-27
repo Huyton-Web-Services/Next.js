62cbdcf2adc129796f339c62dac047d8dae64793
Dave Slack <davidkslack@yahoo.co.uk>
2024-09-27 16:45:16 +0100

    606: Added the more to the main menu

--- components/mainMenu/MainMenu.js
+++ components/mainMenu/MainMenu.js
@@ -5,8 +5,47 @@ import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import {useEffect, useRef, useState} from "react";
+import Popper from '@mui/material/Popper';
+import Grow from '@mui/material/Grow';
+import MenuItem from '@mui/material/MenuItem';
+import MenuList from '@mui/material/MenuList';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Paper from '@mui/material/Paper';
 
 export function MainMenu({mainMenu, articlesMenu}){
+    const [open, setOpen] = useState(false);
+    const anchorRef = useRef(null);
+
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
+    };
+    const handleClose = (event) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    function handleListKeyDown(event) {
+        if (event.key === 'Tab') {
+            event.preventDefault();
+            setOpen(false);
+        } else if (event.key === 'Escape') {
+            setOpen(false);
+        }
+    }
+
+    const prevOpen = useRef(open);
+    useEffect(() => {
+        if (prevOpen.current === true && open === false) {
+            anchorRef.current.focus();
+        }
+
+        prevOpen.current = open;
+    }, [open]);
+
     return(
         <>
             <AppBar position="static">
@@ -21,24 +60,61 @@ export function MainMenu({mainMenu, articlesMenu}){
                     <Divider />
                     <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
                         {mainMenu.map((link) => (
-                            <Link href={link.href}>
-                                <Button key={link.href} sx={{ color: '#fff' }}>
+                            <Link key={link.href} href={link.href}>
+                                <Button sx={{ color: '#fff' }}>
                                     {link.title}
                                 </Button>
                             </Link>
                         ))}
-
                         <Button
                             sx={{ color: '#fff' }}
+                            ref={anchorRef}
+                            id="composition-button"
+                            aria-controls={open ? 'composition-menu' : undefined}
+                            aria-expanded={open ? 'true' : undefined}
+                            aria-haspopup="true"
+                            onClick={handleToggle}
                         >
                             Articles
                         </Button>
-
+                        <Popper
+                            open={open}
+                            anchorEl={anchorRef.current}
+                            role={undefined}
+                            placement="bottom-start"
+                            transition
+                            disablePortal
+                        >
+                            {({ TransitionProps, placement }) => (
+                                <Grow
+                                    {...TransitionProps}
+                                    style={{
+                                        transformOrigin:
+                                            placement === 'bottom-start' ? 'left top' : 'left bottom',
+                                    }}
+                                >
+                                    <Paper>
+                                        <ClickAwayListener onClickAway={handleClose}>
+                                            <MenuList
+                                                autoFocusItem={open}
+                                                id="composition-menu"
+                                                aria-labelledby="composition-button"
+                                                onKeyDown={handleListKeyDown}
+                                            >
+                                                {articlesMenu.map((link) => (
+                                                    <MenuItem key={link.href} onClick={handleClose}><Link href={link.href}>{link.title}</Link></MenuItem>
+                                                ))}
+                                            </MenuList>
+                                        </ClickAwayListener>
+                                    </Paper>
+                                </Grow>
+                            )}
+                        </Popper>
                     </Box>
                 </Toolbar>
 
             </AppBar>
-
+            {/*
             <nav>
                 <h3>Main Menu</h3>
                 <ul>
@@ -58,6 +134,7 @@ export function MainMenu({mainMenu, articlesMenu}){
                     </li>
                 </ul>
             </nav>
+            */}
         </>
     );
 }
