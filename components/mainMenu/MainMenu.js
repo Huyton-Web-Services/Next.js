4f736e2a14b90ddd4df91ab3e4517ac30e9e4b80
Dave Slack <davidkslack@yahoo.co.uk>
2024-09-27 16:46:49 +0100

    606: Cleaned code

--- components/mainMenu/MainMenu.js
+++ components/mainMenu/MainMenu.js
@@ -47,94 +47,70 @@ export function MainMenu({mainMenu, articlesMenu}){
     }, [open]);
 
     return(
-        <>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography
-                        variant="h6"
-                        component="div"
-                        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+        <AppBar position="static">
+            <Toolbar>
+                <Typography
+                    variant="h6"
+                    component="div"
+                    sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                >
+                    Tramp Creative
+                </Typography>
+                <Divider />
+                <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                    {mainMenu.map((link) => (
+                        <Link key={link.href} href={link.href}>
+                            <Button sx={{ color: '#fff' }}>
+                                {link.title}
+                            </Button>
+                        </Link>
+                    ))}
+                    <Button
+                        sx={{ color: '#fff' }}
+                        ref={anchorRef}
+                        id="composition-button"
+                        aria-controls={open ? 'composition-menu' : undefined}
+                        aria-expanded={open ? 'true' : undefined}
+                        aria-haspopup="true"
+                        onClick={handleToggle}
                     >
-                        Tramp Creative
-                    </Typography>
-                    <Divider />
-                    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-                        {mainMenu.map((link) => (
-                            <Link key={link.href} href={link.href}>
-                                <Button sx={{ color: '#fff' }}>
-                                    {link.title}
-                                </Button>
-                            </Link>
-                        ))}
-                        <Button
-                            sx={{ color: '#fff' }}
-                            ref={anchorRef}
-                            id="composition-button"
-                            aria-controls={open ? 'composition-menu' : undefined}
-                            aria-expanded={open ? 'true' : undefined}
-                            aria-haspopup="true"
-                            onClick={handleToggle}
-                        >
-                            Articles
-                        </Button>
-                        <Popper
-                            open={open}
-                            anchorEl={anchorRef.current}
-                            role={undefined}
-                            placement="bottom-start"
-                            transition
-                            disablePortal
-                        >
-                            {({ TransitionProps, placement }) => (
-                                <Grow
-                                    {...TransitionProps}
-                                    style={{
-                                        transformOrigin:
-                                            placement === 'bottom-start' ? 'left top' : 'left bottom',
-                                    }}
-                                >
-                                    <Paper>
-                                        <ClickAwayListener onClickAway={handleClose}>
-                                            <MenuList
-                                                autoFocusItem={open}
-                                                id="composition-menu"
-                                                aria-labelledby="composition-button"
-                                                onKeyDown={handleListKeyDown}
-                                            >
-                                                {articlesMenu.map((link) => (
-                                                    <MenuItem key={link.href} onClick={handleClose}><Link href={link.href}>{link.title}</Link></MenuItem>
-                                                ))}
-                                            </MenuList>
-                                        </ClickAwayListener>
-                                    </Paper>
-                                </Grow>
-                            )}
-                        </Popper>
-                    </Box>
-                </Toolbar>
-
-            </AppBar>
-            {/*
-            <nav>
-                <h3>Main Menu</h3>
-                <ul>
-                    {mainMenu.map((link)=>{
-                        return (
-                            <li key={link.href}><Link href={link.href}>{link.title}</Link></li>
-                        );})
-                    }
-                    <li>Articles
-                        <ul>
-                            {articlesMenu.map((link)=>{
-                                return (
-                                    <li key={link.href}><Link href={link.href}>{link.title}</Link></li>
-                                );})
-                            }
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
-            */}
-        </>
+                        Articles
+                    </Button>
+                    <Popper
+                        open={open}
+                        anchorEl={anchorRef.current}
+                        role={undefined}
+                        placement="bottom-start"
+                        transition
+                        disablePortal
+                    >
+                        {({ TransitionProps, placement }) => (
+                            <Grow
+                                {...TransitionProps}
+                                style={{
+                                    transformOrigin:
+                                        placement === 'bottom-start' ? 'left top' : 'left bottom',
+                                }}
+                            >
+                                <Paper>
+                                    <ClickAwayListener onClickAway={handleClose}>
+                                        <MenuList
+                                            autoFocusItem={open}
+                                            id="composition-menu"
+                                            aria-labelledby="composition-button"
+                                            onKeyDown={handleListKeyDown}
+                                        >
+                                            {articlesMenu.map((link) => (
+                                                <MenuItem key={link.href} onClick={handleClose}><Link href={link.href}>{link.title}</Link></MenuItem>
+                                            ))}
+                                        </MenuList>
+                                    </ClickAwayListener>
+                                </Paper>
+                            </Grow>
+                        )}
+                    </Popper>
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
 }
