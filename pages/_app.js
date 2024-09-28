e16685a06018f96c14e309c405b3381e12c8b31f
Dave Slack <davidkslack@yahoo.co.uk>
2024-09-28 14:31:34 +0100

    606: Added custom colours

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,25 @@ import Head from "next/head";
 export const apiDomain = "https://cms.trampcreative.co.uk"; // Use CMS domain
 export const domain = "https://www.trampcreative.co.uk"; // Use your domain
 
+// Main colour #f1e4d4
+let myColour;
+myColour = [
+    "#fef4eb",
+    "#f3e9db",
+    "#e6d0b7",
+    "#d7b68e",
+    "#cca16c",
+    "#c49356",
+    "#c18c4a",
+    "#aa773b",
+    "#986a31",
+    "#855a26"
+];
+
 const theme = createTheme({
-    /** Put your mantine theme override here */
+    colors: {
+        myColour,
+    }
 });
 
 export default function MyApp({ Component, pageProps }) {
