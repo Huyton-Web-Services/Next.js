f0c08b11f69052cafbfdbbfbcc0c0c737e532bf0
Dave Slack <davidkslack@yahoo.co.uk>
2024-10-01 18:36:31 +0100

    606: Added the nav bar

--- components/mainMenu/MainMenu.js
+++ components/mainMenu/MainMenu.js
@@ -1,25 +1,36 @@
 import Link from "next/link";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export function MainMenu({mainMenu, articlesMenu}){
-    return(
-        <nav>
-            <h3>Main Menu</h3>
-            <ul>
-                {mainMenu.map((link)=>{
-                    return (
-                        <li key={link.href}><Link href={link.href}>{link.title}</Link></li>
-                    );})
-                }
-                <li>Articles
-                    <ul>
-                        {articlesMenu.map((link)=>{
+
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container fluid>
+                <Navbar.Brand href="#"><Link href={'/'}>Curabitur</Link></Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav className="ms-auto my-2 my-lg-0" >
+
+                        {mainMenu.map((link)=>{
                             return (
-                                <li key={link.href}><Link href={link.href}>{link.title}</Link></li>
+                                <Nav.Link key={link.href} href={link.href}>{link.title}</Nav.Link>
                             );})
                         }
-                    </ul>
-                </li>
-            </ul>
-        </nav>
+                        <NavDropdown title="Articles" id="navbarScrollingDropdown">
+
+                            {articlesMenu.map((link)=>{
+                                return (
+                                    <NavDropdown.Item key={link.href} href={link.href}>{link.title}</NavDropdown.Item>
+                                );})
+                            }
+
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
