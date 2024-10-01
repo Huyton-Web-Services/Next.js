549f2e2a6597411059dbe8705e433e0805119451
Dave Slack <davidkslack@yahoo.co.uk>
2024-10-01 17:39:57 +0100

    606: Added the image and layout

--- pages/[[...folder]][[...page]].js
+++ pages/[[...folder]][[...page]].js
@@ -1,9 +1,13 @@
 import {getContent} from "@/data/content";
 import {getMenu} from "@/data/menu";
 import {MainMenu} from "@/components/mainMenu/MainMenu";
-import Image from "next/image";
+//import Image from "next/image";
+import Image from 'react-bootstrap/Image';
 import Head from "next/head";
 import {domain} from "@/pages/_app";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export async function getServerSideProps({resolvedUrl}) {
     const content = await getContent(resolvedUrl);
@@ -28,19 +32,38 @@ export default function FolderPage({ content, mainMenu, resolvedUrl, articlesMen
                     `}</style>
                 }
             </Head>
+
             <MainMenu mainMenu={mainMenu} articlesMenu={articlesMenu} />
-            {content.main_image &&
-                <Image
-                    src={content.main_image.url}
-                    width={content.main_image.width}
-                    height={content.main_image.height}
-                    alt={content.main_image.alt}
-                    priority={true}
-                />
-            }
-            <h1>{content.title}</h1>
-            <h2>{content.heading}</h2>
-            <div dangerouslySetInnerHTML={{ __html: content.body }} />
+
+            <Container>
+                <Row>
+                    <Col md={12} lg={8}>
+                        <h1>{content.title}</h1>
+                    </Col>
+                    <Col md={12} lg={4}>
+                        <p>{content.heading}</p>
+                    </Col>
+                </Row>
+            </Container>
+
+            <Container>
+                {content.main_image ?
+                    <Row>
+                        <Col md={12} lg={6}>
+                            <Image
+                                src={content.main_image.url}
+                                alt={content.main_image.alt}
+                                fluid
+                            />
+                        </Col>
+                        <Col md={12} lg={6}>
+                            <div dangerouslySetInnerHTML={{ __html: content.body }} />
+                        </Col>
+                    </Row>
+                    :
+                    <div dangerouslySetInnerHTML={{ __html: content.body }} />
+                }
+            </Container>
         </>
     );
 }
