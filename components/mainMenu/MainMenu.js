9ac8beba29e76c82c5c74b42af444c916b2a53fb
Dave Slack <davidkslack@yahoo.co.uk>
2024-10-02 10:01:18 +0100

    606: Fixed the <a> in <a> issue on the navbar brand

--- components/mainMenu/MainMenu.js
+++ components/mainMenu/MainMenu.js
@@ -1,32 +1,27 @@
-import Link from "next/link";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export function MainMenu({mainMenu, articlesMenu}){
-
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container fluid>
-                <Navbar.Brand href="#"><Link href={'/'}>Curabitur</Link></Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
+                <Navbar.Brand href="/">Curabitur</Navbar.Brand>
+                <Navbar.Toggle aria-controls="mainMenuScroll" />
+                <Navbar.Collapse id="mainMenuScroll">
                     <Nav className="ms-auto my-2 my-lg-0" >
-
                         {mainMenu.map((link)=>{
                             return (
                                 <Nav.Link key={link.href} href={link.href}>{link.title}</Nav.Link>
                             );})
                         }
-                        <NavDropdown title="Articles" id="navbarScrollingDropdown">
-
+                        <NavDropdown align="end" title="Articles" id="mainMenuDropdown">
                             {articlesMenu.map((link)=>{
                                 return (
                                     <NavDropdown.Item key={link.href} href={link.href}>{link.title}</NavDropdown.Item>
                                 );})
                             }
-
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
