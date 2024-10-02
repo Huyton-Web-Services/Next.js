8faa21b26c6d109bbf84d7086d694469281e11c1
Dave Slack <davidkslack@yahoo.co.uk>
2024-10-02 16:57:03 +0100

    606: Added the main menu

--- components/mainMenu/MainMenu.js
+++ components/mainMenu/MainMenu.js
@@ -1,25 +1,146 @@
-import Link from "next/link";
+import { useState } from 'react'
+import {
+    Dialog,
+    DialogPanel,
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    Popover,
+    PopoverButton,
+    PopoverGroup,
+    PopoverPanel,
+} from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
 export function MainMenu({mainMenu, articlesMenu}){
-    return(
-        <nav>
-            <h3>Main Menu</h3>
-            <ul>
-                {mainMenu.map((link)=>{
-                    return (
-                        <li key={link.href}><Link href={link.href}>{link.title}</Link></li>
-                    );})
-                }
-                <li>Articles
-                    <ul>
-                        {articlesMenu.map((link)=>{
-                            return (
-                                <li key={link.href}><Link href={link.href}>{link.title}</Link></li>
-                            );})
-                        }
-                    </ul>
-                </li>
-            </ul>
-        </nav>
-    );
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+console.log(articlesMenu);
+    return (
+        <header className="bg-black">
+            <nav aria-label="Global" className="flex max-w-full items-center justify-between p-2 lg:px-8">
+                <div className="flex lg:flex-1">
+                    <a href="/" className="text-4xl">
+                        Curabitur
+                    </a>
+                </div>
+                <div className="flex lg:hidden">
+                    <button
+                        type="button"
+                        onClick={() => setMobileMenuOpen(true)}
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+                    </button>
+                </div>
+                <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+
+                    {mainMenu.map((item) => (
+                        <a href={item.href} className="text-sm font-semibold leading-6 text-white">
+                            {item.title}
+                        </a>
+                    ))}
+
+                    <Popover className="relative">
+                        <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold leading-6">
+                            Articles
+                            <ChevronDownIcon aria-hidden="true" className="h-5 w-5 flex-none text-gray-400" />
+                        </PopoverButton>
+
+                        <PopoverPanel
+                            transition
+                            className="absolute -left-96 top-6 z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-black shadow-lg ring-1 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
+                        >
+                            <div className="p-4">
+                                {articlesMenu.map((item) => (
+                                    <div
+                                        key={item.title}
+                                        className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-blue-950"
+                                    >
+
+                                        <div className="flex-auto">
+                                            <a href={item.href} className="block font-semibold text-white">
+                                                {item.title}
+                                                <span className="absolute inset-0" />
+                                            </a>
+
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+
+                        </PopoverPanel>
+                    </Popover>
+
+                </PopoverGroup>
+
+            </nav>
+            <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+                <div className="fixed inset-0 z-10" />
+                <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <div className="flex items-center justify-between">
+                        <a href="#" className="-m-1.5 p-1.5">
+                            <span className="sr-only">Your Company</span>
+                            <img
+                                alt=""
+                                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                                className="h-8 w-auto"
+                            />
+                        </a>
+                        <button
+                            type="button"
+                            onClick={() => setMobileMenuOpen(false)}
+                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                        >
+                            <span className="sr-only">Close menu</span>
+                            <XMarkIcon aria-hidden="true" className="h-6 w-6" />
+                        </button>
+                    </div>
+                    <div className="mt-6 flow-root">
+                        <div className="-my-6 divide-y divide-gray-500/10">
+                            <div className="space-y-2 py-6">
+                                <Disclosure as="div" className="-mx-3">
+                                    <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                                        Product
+                                        <ChevronDownIcon aria-hidden="true" className="h-5 w-5 flex-none group-data-[open]:rotate-180" />
+                                    </DisclosureButton>
+                                    <DisclosurePanel className="mt-2 space-y-2">
+                                        {[...articlesMenu].map((item) => (
+                                            <DisclosureButton
+                                                key={item.title}
+                                                as="a"
+                                                href={item.href}
+                                                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                            >
+                                                {item.title}
+                                            </DisclosureButton>
+                                        ))}
+                                    </DisclosurePanel>
+                                </Disclosure>
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Features
+                                </a>
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Marketplace
+                                </a>
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Company
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </DialogPanel>
+            </Dialog>
+        </header>
+    )
 }
