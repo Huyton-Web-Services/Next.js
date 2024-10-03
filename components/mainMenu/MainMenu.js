5554c94214dde1035396763c92aaab7ce4523505
Dave Slack <davidkslack@yahoo.co.uk>
2024-10-03 13:30:50 +0100

    606: Updated the main menu to use the Link component

--- components/mainMenu/MainMenu.js
+++ components/mainMenu/MainMenu.js
@@ -12,17 +12,22 @@ import {
 } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import Link from "next/link";
 
 export function MainMenu({mainMenu, articlesMenu}){
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-console.log(articlesMenu);
+
     return (
         <header className="bg-black">
             <nav aria-label="Global" className="flex max-w-full items-center justify-between p-2 lg:px-8">
                 <div className="flex lg:flex-1">
-                    <a href="/" className="text-4xl">
+                    <Link
+                        href="/"
+                        onClick={() => setMobileMenuOpen(false)}
+                        className="text-4xl"
+                    >
                         Curabitur
-                    </a>
+                    </Link>
                 </div>
                 <div className="flex lg:hidden">
                     <button
@@ -37,9 +42,14 @@ console.log(articlesMenu);
                 <PopoverGroup className="hidden lg:flex lg:gap-x-12">
 
                     {mainMenu.map((item) => (
-                        <a href={item.href} className="text-sm font-semibold leading-6 text-white">
+                        <Link
+                            key={item.href}
+                            onClick={() => setMobileMenuOpen(false)}
+                            href={item.href}
+                            className="text-sm font-semibold leading-6 text-white"
+                        >
                             {item.title}
-                        </a>
+                        </Link>
                     ))}
 
                     <Popover className="relative">
@@ -60,10 +70,14 @@ console.log(articlesMenu);
                                     >
 
                                         <div className="flex-auto">
-                                            <a href={item.href} className="block font-semibold text-white">
+                                            <Link
+                                                href={item.href}
+                                                onClick={() => setMobileMenuOpen(false)}
+                                                className="block font-semibold text-white"
+                                            >
                                                 {item.title}
                                                 <span className="absolute inset-0" />
-                                            </a>
+                                            </Link>
 
                                         </div>
                                     </div>
@@ -78,20 +92,19 @@ console.log(articlesMenu);
             </nav>
             <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
                 <div className="fixed inset-0 z-10" />
-                <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
-                        <a href="#" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
-                            <img
-                                alt=""
-                                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                                className="h-8 w-auto"
-                            />
-                        </a>
+                        <Link
+                            href="/"
+                            onClick={() => setMobileMenuOpen(false)}
+                            className="text-4xl"
+                        >
+                            Curabitur
+                        </Link>
                         <button
                             type="button"
                             onClick={() => setMobileMenuOpen(false)}
-                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                            className="-m-2.5 rounded-md p-2.5 text-white"
                         >
                             <span className="sr-only">Close menu</span>
                             <XMarkIcon aria-hidden="true" className="h-6 w-6" />
@@ -100,9 +113,20 @@ console.log(articlesMenu);
                     <div className="mt-6 flow-root">
                         <div className="-my-6 divide-y divide-gray-500/10">
                             <div className="space-y-2 py-6">
+
+                                {mainMenu.map((item) => (
+                                    <Link
+                                        onClick={() => setMobileMenuOpen(false)}
+                                        href={item.href}
+                                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-blue-950"
+                                    >
+                                        {item.title}
+                                    </Link>
+                                ))}
+
                                 <Disclosure as="div" className="-mx-3">
-                                    <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                                        Product
+                                    <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-white hover:bg-blue-950">
+                                        Articles
                                         <ChevronDownIcon aria-hidden="true" className="h-5 w-5 flex-none group-data-[open]:rotate-180" />
                                     </DisclosureButton>
                                     <DisclosurePanel className="mt-2 space-y-2">
@@ -110,32 +134,15 @@ console.log(articlesMenu);
                                             <DisclosureButton
                                                 key={item.title}
                                                 as="a"
+                                                onClick={() => setMobileMenuOpen(false)}
                                                 href={item.href}
-                                                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-white hover:bg-blue-950"
                                             >
                                                 {item.title}
                                             </DisclosureButton>
                                         ))}
                                     </DisclosurePanel>
                                 </Disclosure>
-                                <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                >
-                                    Features
-                                </a>
-                                <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                >
-                                    Marketplace
-                                </a>
-                                <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                >
-                                    Company
-                                </a>
                             </div>
                         </div>
                     </div>
